5b95cdd3d5b3e9ee5b03026cf042364170b264a2
monpro <monproshen@gmail.com>
2020-09-29 20:23:44 +1000

    add constants and enums for libs oop design

---
+++ oop/library/contants.ts
@@ -0,0 +1,51 @@
+export enum BookFormat {
+    HARDCOVER,
+    PAPERBACK,
+    EBOOK,
+    NEWSPAPER
+}
+
+export enum BookStatus {
+    AVAILABLE,
+    RESERVED,
+    LOANED,
+    LOST
+}
+
+export enum ReservationStatus {
+    WAITING,
+    PENDING,
+    CANCELED,
+    NONE
+}
+
+export enum AccountStatus {
+    ACTIVE,
+    CLOSED,
+    CANCELED,
+    NONE
+}
+
+
+export class Address {
+    private streetAddress: string;
+    private city: string;
+    private state: string;
+    private zipCode: string;
+    private country: string;
+}
+
+export class Person {
+    private name: string;
+    private address: Address;
+    private email: string;
+    private phone: string;
+}
+
+export class BooksConstants {
+    static readonly MAX_BOOKS_TO_ONE_USER =10;
+    static readonly MAX_LENDING_DAYS = 10;
+}
+
+
+
