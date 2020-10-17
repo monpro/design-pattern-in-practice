4f8ee7eba54224b65902da7414635bd5417774cf
monpro <monproshen@gmail.com>
2020-10-18 10:53:39 +1100

    use Queue (FIFO) to speed up multi threads crawler

--- async/crawler.py
+++ async/crawler.py
@@ -1,6 +1,7 @@
 import time
 import threading
 import requests
+import queue as q
 
 
 def main():
@@ -12,15 +13,32 @@ def main():
       link_list.append(link)
   start = time.time()
 
-  link_range_list = [(1, 75), (76, 150), (151, 225), (226, 280)]
-  thread_list = []
-  for i in range(len(link_range_list)):
-    thread = CrawlerThread('Thread {0}'.format(i + 1), link_range_list[i], link_list)
+  thread_list = ['thread - {0}'.format(i) for i in range(1, 6)]
+  queue = q.Queue(1000)
+  threads = []
+  for name in thread_list:
+    thread = CrawlerThread(name, queue)
     thread.start()
-    thread_list.append(thread)
-  for thread in thread_list:
+    threads.append(thread)
+
+  for url in link_list:
+    queue.put(url)
+
+  for thread in threads:
     thread.join()
+
   end = time.time()
+  print('total time is {0}'.format(end - start))
+
+  # link_range_list = [(1, 75), (76, 150), (151, 225), (226, 280)]
+  # thread_list = []
+  # for i in range(len(link_range_list)):
+  #   thread = CrawlerThread('Thread {0}'.format(i + 1), link_range_list[i], link_list)
+  #   thread.start()
+  #   thread_list.append(thread)
+  # for thread in thread_list:
+  #   thread.join()
+  # end = time.time()
 
   # for link in link_list:
   #   try:
@@ -28,33 +46,42 @@ def main():
   #     print(res.status_code, link)
   #   except Exception as e:
   #     print('Error', e)
-  # end = time.time()
-
-  print('total time is {0}'.format(end - start))
 
 
 class CrawlerThread(threading.Thread):
-  def __init__(self, name, link_range, link_list):
+  def __init__(self, name, queue):
     threading.Thread.__init__(self)
     self.name = name
-    self.link_range = link_range
-    self.link_list = link_list
-    print(self.link_list)
-    print(self.link_range)
+    self.queue = queue
+    # self.link_range = link_range
+    # self.link_list = link_list
 
   def run(self):
-    print('starting ' + self.name)
-    crawler(self.name, self.link_range, self.link_list)
-    print('exiting ' + self.name)
+    while True:
+      try:
+        print('starting ' + self.name)
+        crawler(self.name, self.queue)
+        print('exiting ' + self.name)
+      except Exception as e:
+        print(e)
+        break
 
 
-def crawler(name, link_range, link_list):
-  for i in range(link_range[0], link_range[1]):
-    try:
-      res = requests.get(link_list[i], timeout=20)
-      print(name, res.status_code, link_list[i])
-    except Exception as e:
-      print(name, 'Error: ', e)
+# def crawler(name, link_range, link_list):
+#   for i in range(link_range[0], link_range[1]):
+#     try:
+#       res = requests.get(link_list[i], timeout=20)
+#       print(name, res.status_code, link_list[i])
+#     except Exception as e:
+#       print(name, 'Error: ', e)
+
+def crawler(name, worker_queue):
+  url = worker_queue.get(timeout=2)
+  try:
+    res = requests.get(url, timeout=20)
+    print(worker_queue.qsize(), name, res.status_code, url)
+  except Exception as e:
+    print(worker_queue.qsize(), name, url, "error: ", e)
 
 
 if __name__ == "__main__":
