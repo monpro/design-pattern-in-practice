59771373ff3f37265eb20bc8e6aff4637adbb4d7
monpro <monproshen@gmail.com>
2020-10-21 17:04:42 +1100

    use Barrier Semaphore and Lock to implement seated function

--- multithread/uber_ride.py
+++ multithread/uber_ride.py
@@ -1,11 +1,13 @@
-from threading import Lock, Barrier, current_thread
+from threading import Lock, Barrier, current_thread, Semaphore
 
 
 class UberRide:
 
   def __init__(self):
     self.dc_count = 0
+    self.dc_waiting = Semaphore(0)
     self.marvel_count = 0
+    self.marvel_waiting = Semaphore(0)
     self.lock = Lock()
     self.barrier = Barrier(4)
     self.ride_count = 0
@@ -18,8 +20,63 @@ class UberRide:
     print("{0} {1} seated".format(group, current_thread().getName()))
 
   def seat_dc(self):
-    pass
+    ride_leader = False
+
+    self.lock.acquire()
+    self.dc_count += 1
+
+    if self.dc_count == 4:
+      self.dc_waiting.release()
+      self.dc_waiting.release()
+      self.dc_waiting.release()
+
+      self.dc_count -= 4
+      ride_leader = True
+    elif self.dc_count == 2 and self.marvel_count >= 2:
+      self.dc_waiting.release()
+      self.marvel_waiting.release()
+      self.marvel_waiting.release()
+
+      self.dc_count -= 2
+      self.marvel_count -= 2
+      ride_leader = True
+    else:
+      self.lock.release()
+      self.dc_waiting.acquire()
+
+    self.seated("dc")
+    self.barrier.wait()
+
+    if ride_leader is True:
+      self.drive()
+      self.lock.release()
 
   def seat_marvel(self):
-    pass
+    ride_leader = False
+    self.lock.acquire()
+    self.marvel_count += 1
+    if self.marvel_count == 4:
+      self.marvel_waiting.release()
+      self.marvel_waiting.release()
+      self.marvel_waiting.release()
+
+      self.marvel_count -= 4
+      ride_leader = True
+    elif self.marvel_count == 2 and self.dc_count >= 2:
+      self.marvel_waiting.release()
+      self.dc_waiting.release()
+      self.dc_waiting.release()
+
+      self.marvel_count -= 2
+      self.dc_count -= 2
+    else:
+      self.lock.release()
+      self.marvel_waiting.acquire()
+
+    self.seated("marvel")
+    self.barrier.wait()
+
+    if ride_leader is True:
+      self.drive()
+      self.lock.release()
 
