39ad9afd642c2e3cbe559c55a4ad5a6110da7300
monpro <monproshen@gmail.com>
2020-10-22 19:54:52 +1100

    add max dinner semaphore using re-entrance lock to avoid dead-lock

--- multithread/philosopher_dinning.py
+++ multithread/philosopher_dinning.py
@@ -1,12 +1,13 @@
 import random
 import time
-from threading import Semaphore
+from threading import Semaphore, Thread
 
 
 class PhilosopherDinning:
 
   def __init__(self):
     self.forks = [Semaphore(1) for _ in range(5)]
+    self.max_dinner = Semaphore(4)
 
   def life_cycle_philosopher(self, phi_id):
     while True:
@@ -18,6 +19,10 @@ class PhilosopherDinning:
     time.sleep(sleep_time)
 
   def eat(self, phi_id):
+    # re-entrance lock
+    # only 4 philosophers could grab the fork at most
+    self.max_dinner.acquire()
+
     # phi0: need fork 4 and 0
     # phi1: need fork 0 and 1
     # left (phi_id + 4) % 5, right phi_id
@@ -31,3 +36,19 @@ class PhilosopherDinning:
 
     self.forks[left_fork].release()
     self.forks[right_fork].release()
+
+    self.max_dinner.release()
+
+
+if __name__ == "__main__":
+  philosopher_dinning = PhilosopherDinning()
+  threads = list()
+  for index in range(5):
+    threads.append(Thread(target=philosopher_dinning.life_cycle_philosopher, args=(index,)))
+
+  for thread in threads:
+    thread.start()
+
+  for thread in threads:
+    thread.join()
+
