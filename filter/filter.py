c5a662733f46b5451a380decaead41fff4f2b3d7
monpro <monproshen@gmail.com>
2020-10-28 23:06:25 +1100

    add filter pattern framework example

---
+++ filter/filter.py
@@ -0,0 +1,25 @@
+from abc import ABCMeta, abstractmethod
+
+
+class Filter(ABCMeta):
+
+  @abstractmethod
+  def doFilter(self, elements):
+    pass
+
+
+class FilterChain(Filter):
+
+  def __init__(self):
+    self._filters = []
+
+  def addFilter(self, _filter):
+    self._filters.append(_filter)
+
+  def removeFilter(self, _filter):
+    self._filters.remove(_filter)
+
+  def doFilter(self, elements):
+    for _filter in self._filters:
+      elements = _filter.doFilter(elements)
+    return elements
